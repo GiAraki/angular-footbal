5a36b53dc356af626c01b5f5ad4bb8640f7594f6
Giovanni Sarao Araki <giovanni.saraoaraki@gmail.com>
2023-11-13 10:48:46 -0300

    feature: lista-detail + navbar

--- src/app/components/nav-bars/nav-bar.component.ts
+++ src/app/components/nav-bars/nav-bar.component.ts
@@ -1,36 +1,52 @@
-import { League } from './../../models/standings-response';
-import { HttpParams } from '@angular/common/http';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Params, Router } from '@angular/router';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NavBarLinks } from 'src/app/models/nav-bar-links';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports:[MatButtonToggleModule, FormsModule, BrowserModule ],
+  imports: [MatButtonToggleModule, FormsModule, BrowserModule],
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent implements OnInit, OnDestroy {
   @Input() links: NavBarLinks[] = [];
   @Output() newLinkEvent: EventEmitter<number> = new EventEmitter<number>();
-
+  exibirComponente: boolean = true;
+  private destroy$: Subject<void> = new Subject<void>();
   selectedButton: string = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit() {
-    this.selectedButton =  this.links[0].id;
+  ngOnInit(): void {
+    this.route.params
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((params: Params) => {
+        this.exibirComponente = !params['id'];
+      });
+
+    this.selectedButton = this.links[0].id;
     this.changeLink(this.links[0].leagueId);
   }
 
-  changeLink(leagueId: number): void{
+  changeLink(leagueId: number): void {
     this.newLinkEvent.emit(leagueId);
   }
 
-
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
